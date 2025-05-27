4ec1d9a9e17574ebefa4f2eb9105e934213fb7eb
LeoDoCa <20223tn049@utez.edu.mx>
2025-05-27 15:50:43 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/cvlddc9b/Cvlddc9bApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvlddc9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvlddc9bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvlddc9bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/cvlddc9b/Cvlddc9bApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvlddc9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvlddc9bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
